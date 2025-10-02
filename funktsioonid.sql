f09c88851ea1080d2f6c2aab3f6e2d654d936ed8
polopo232 <opilane@HP-CZC2349HT7>
2025-10-02 16:27:24 +0300

    Osa 32. Funktsioonid

---
+++ funktsioonid.sql
@@ -0,0 +1,12 @@
+--Funktsioonid
+
+SELECT * FROM DimEmployeeS
+
+CREATE FUNCTION fn_ILTVF_GetEmployees()
+RETURNS TABLE
+AS
+RETURN (SELECT EmployeeKey, FirstName, CAST(BirthDate AS DATE) AS DOB
+FROM dbo.DimEmployee);
+
+SELECT * FROM fn_ILTVF_GetEmployees()
+
