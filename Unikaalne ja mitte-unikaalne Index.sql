cf48b7815b2b70b686b6207227ab2602d75afa59
polopo232 <opilane@tthk.ee>
2025-10-06 11:52:02 +0300

    Loo uue indeksid ja uuendasin vana indeksid. Loon kommentaarid

--- Unikaalne ja mitte-unikaalne Index.sql
+++ Unikaalne ja mitte-unikaalne Index.sql
@@ -12,3 +12,17 @@ EXECUTE sp_helpindex tblEmployee
 
 Insert into tblEmployee Values(1, 'Mike', 'Sandoz', 4500, 'Male', 'New York')
 Insert into tblEmployee Values(1, 'John', 'Menco', 2500, 'Male', 'London')
+
+-- Loon unikaalse mitteklastriindeksi ees- ja perekonnanime j‰rgi
+Create Unique NonClustered Index UIX_tblEmployee_FirstName_LastName
+On tblEmployee(FirstName, LastName)
+
+--Loon mitte unikaalse ja mitte klastritiseeritud indeksi
+ALTER TABLE tblEmployee 
+ADD CONSTRAINT UQ_tblEmployee_City 
+UNIQUE NONCLUSTERED (City)
+
+--- Lisan veerule City unikaalse ja mitteklastriindeksi, et v‰‰rtused oleksid unikaalsed.
+CREATE UNIQUE INDEX IX_tblEmployee_City
+ON tblEmployee(City)
+WITH IGNORE_DUP_KEY
