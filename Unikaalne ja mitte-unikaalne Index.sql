eae5a6a2725753812ff7315a0ccc97ad1de12230
polopo232 <opilane@tthk.ee>
2025-10-06 11:00:07 +0300

    Loo uus fail, uuendamine tabeli tblEbployee ja execute

---
+++ Unikaalne ja mitte-unikaalne Index.sql
@@ -0,0 +1,11 @@
+CREATE TABLE [tblEmployee]
+(
+[Id] int Primary Key,
+[FirstName] nvarchar(50),
+[LastName] nvarchar(50),
+[Salary] int,
+[Gender] nvarchar(10),
+[City] nvarchar(50)
+)
+
+EXECUTE sp_helpindex tblEmployee
