fe70239eed9601d549549549690bea49209f371e
polopo232 <opilane@tthk.ee>
2025-10-06 09:54:42 +0300

    Ajutiste tabelite uus funktsioon spCreateLocalTempTable

--- funktsioonid.sql
+++ funktsioonid.sql
@@ -77,3 +77,13 @@ select * from #PersonDetails
 
 Select name from tempdb..sysobjects
 where name like '#PersonDetails%'
+
+Create Procedure spCreateLocalTempTable
+as
+Begin
+Create Table #PersonDetails (Id int, Name nvarchar(20))
+Insert into #PersonDetails Values(1, 'Mike')
+Insert into #PersonDetails Values (2, 'John')
+Insert into #PersonDetails Values (3, 'Todd')
+Select * from #PersonDetails
+End
