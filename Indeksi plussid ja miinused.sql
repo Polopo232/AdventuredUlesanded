33432f6fec39e1dc36a845808b652af68722077b
polopo232 <opilane@tthk.ee>
2025-10-06 12:33:01 +0300

    Kasutasin DELETE, UPDATE, SELECT tabelite vaatamiseks

--- Indeksi plussid ja miinused.sql
+++ Indeksi plussid ja miinused.sql
@@ -17,3 +17,21 @@ Insert into tblEmployee Values (5, 'James', 'Mirch', 7500, 'Male', 'London')
 --Loomine uus indeks IX_tblEmployee_Salary
 Create NonClustered Index IX_tblEmployee_Salary
 On tblEmployee (Salary Asc)
+
+--Leidsin inimesi, kelle palk on suurem kui 4 tuhat ja väiksem kui 8 tuhat. 
+SELECT * FROM tblEmployee WHERE Salary > 4000 and Salary < 8000
+
+--Kustutasin inimese, kelle palk on 2500
+Delete from tblEmployee where Salary = 2500
+
+--muutis inimese palka 7500-lt 9000-le
+Update tblEmployee Set Salary = 9000 where Salary = 7500
+
+--palga vaatamine sorteerimata ja sorteeritud (DESC)
+SELECT * FROM tblEmployee ORDER BY Salary
+SELECT * FROM tblEmployee ORDER BY Salary DESC
+
+--Näita palka ja nende koguarvu 
+SELECT Salary, COUNT(Salary) AS Total FROM tblEmployee GROUP BY Salary
+
+
