54b393e052a3f2bbf8e891e940c6f375c97c6ffa
polopo232 <opilane@tthk.ee>
2025-10-06 09:14:40 +0300

    .

--- funktsioonid.sql
+++ funktsioonid.sql
@@ -11,14 +11,4 @@ FROM dbo.DimEmployee);
 
 SELECT * FROM fn_ILTVF_GetEmployees()
 
-Create Function fn_MSTVF_GetEmployees()
-Returns @Table Table (Id int, Name nvarchar(50), DOB Date)
-AS
-Begin
-Insert into @Table
-Select EmployeeKey, FirstName + LastName, Cast(BirthDate as Date)
-From DimEmployee
-Return
-End
 
-SELECT * FROM fn_MSTVF_GetEmployees();
