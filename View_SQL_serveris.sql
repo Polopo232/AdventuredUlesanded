5814c7f89c85746e87c386f75790e999e243cc7f
Opilane TTHK <opilane@tthk.ee>
2025-10-09 14:03:02 +0300

    Loomine palju View's näiteks: vWITDepartment_Employees, vWEmployeesNonConfidentialData, vWEmployeesCountByDepartment

--- View_SQL_serveris.sql
+++ View_SQL_serveris.sql
@@ -36,3 +36,35 @@ Select Id, Name, Salary, Gender, DeptName
 from tblEmployee
 join tblDepartment
 on tblEmployee. Departmentld = tblDepartment.Deptld
+
+SELECT * FROM VWEmployeesByDepartment
+
+Create View vWITDepartment_Employees
+as
+Select Id, Name, Salary, Gender, DeptName
+from tblEmployee
+join tblDepartment
+on tblEmployee. Departmentld = tblDepartment.Deptld
+where tblDepartment. DeptName = 'IT'
+
+SELECT * FROM vWITDepartment_Employees
+
+Create View vWEmployeesNonConfidentialData
+as
+Select Id, Name, Gender, DeptName
+from tblEmployee
+join tblDepartment
+on tblEmployee. Departmentld = tblDepartment.Deptld
+
+SELECT * FROM vWEmployeesNonConfidentialData
+
+Create View vWEmployeesCountByDepartment
+as
+Select DeptName, COUNT(Id) as TotalEmployees
+from tblEmployee
+join tblDepartment
+on tblEmployee. Departmentld = tblDepartment.Deptld
+Group By DeptName
+
+SELECT * FROM vWEmployeesCountByDepartment
+
