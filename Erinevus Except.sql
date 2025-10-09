19619e8c70790912fe8a4f60e79eb0f863f90c2c
Polopo232 <nikiforovnikita08@gmail.com>
2025-10-09 15:25:51 +0300

    Veel kaks except käsk

--- Erinevus Except.sql
+++ Erinevus Except.sql
@@ -3,3 +3,12 @@ Select Id, Name, Gender From TableB
 
 Select Id, Name, Gender From TableA
 Where Id NOT IN (Select Id from TableB)
+
+INSERT INTO TableA VALUES (1, 'Mark','Male')
+
+Select Id, Name, Gender From TableA
+Except
+Select Id, Name From TableB
+
+Select Id, Name, Gender From TableA
+Where Id NOT IN (Select Id, Name from TableB)
