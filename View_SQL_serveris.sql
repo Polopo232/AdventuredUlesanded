c1e9a9771569d69882525fa1edf3ed7f2e0e5f5f
Polopo232 <nikiforovnikita08@gmail.com>
2025-10-09 14:39:17 +0300

    Kasuta tabelid

--- View_SQL_serveris.sql
+++ View_SQL_serveris.sql
@@ -1,44 +1,12 @@
-CREATE TABLE tblEmployee
-(
-Id int Primary Key,
-Name nvarchar(30),
-Salary int,
-Gender nvarchar(10),
-Departmentld int
-)
-
-CREATE TABLE tblDepartment
-(
-Deptld int Primary Key,
-DeptName nvarchar(20)
-)
-
-Insert into tblDepartment values (1,'IT')
-Insert into tblDepartment values (2, 'Payroll')
-Insert into tblDepartment values (3,'HR')
-Insert into tblDepartment values (4, 'Admin')
-
-
-
-
-Insert into tblEmployee values (1, 'John', 5000, 'Male', 3)
-Insert into tblEmployee values (2, 'Mike', 3400, 'Male', 2)
-Insert into tblEmployee values (3, 'Pam', 6000, 'Female', 1)
-Insert into tblEmployee values (4, 'Todd', 4800, 'Male', 4)
-Insert into tblEmployee values (5, 'Sara', 3200, 'Female', 1)
-Insert into tblEmployee values (6, 'Ben', 4800, 'Male', 3)
-
-Select Id, Name, Salary, Gender, DeptName
-from tblEmployee
-join tblDepartment
-on tblEmployee. Departmentld = tblDepartment. Deptld
-
-Create View VWEmployeesByDepartment
-as
-Select Id, Name, Salary, Gender, DeptName
-from tblEmployee
-join tblDepartment
-on tblEmployee. Departmentld = tblDepartment.Deptld
+CREATE VIEW VWEmployeesByDepartment AS
+SELECT 
+    e.EmployeeKey AS Id,
+    e.FirstName, 
+	e.LastName AS Name,
+    e.Gender,
+    o.OrganizationName AS DeptName
+FROM DimEmployee e
+JOIN DimOrganization o ON e.EmployeeKey = o.OrganizationKey;
 
 SELECT * FROM VWEmployeesByDepartment
 
