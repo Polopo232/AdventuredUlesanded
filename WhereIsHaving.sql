4bdb20198ac309fb75fb8bad98bf31764cd2c3eb
Polopo232 <nikiforovnikita08@gmail.com>
2025-10-09 15:50:45 +0300

    viimased 2

--- WhereIsHaving.sql
+++ WhereIsHaving.sql
@@ -12,4 +12,18 @@ FROM FactInternetSales
 GROUP BY ProductKey
 WHERE SUM (SalesAmount) > 1000
 
+select EnglishProductName, SUM(UnitPrice) as TotalSales
+from FactInternetSales sale
+join DimProduct prod
+on prod.ProductKey = sale.ProductKey
+where EnglishProductName in ('AWC Logo Cap')
+group by EnglishProductName;
+
+select EnglishProductName, SUM(UnitPrice) as TotalSales
+from FactInternetSales sale
+join DimProduct prod
+on prod.ProductKey = sale.ProductKey
+group by EnglishProductName
+having EnglishProductName in ('AWC Logo Cap');
+
 SELECT * FROM FactInternetSales
