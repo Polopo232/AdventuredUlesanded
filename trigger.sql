5d65ebf479addbba7ba4aadad55f46befb9688b1
Polopo232 <nikiforovnikita08@gmail.com>
2025-10-09 15:30:52 +0300

    Uuendamine myFirstTrigger

--- trigger.sql
+++ trigger.sql
@@ -23,3 +23,12 @@ AS
 BEGIN
 Print 'A table has just been created, modified or deleted'
 END
+
+ALTER TRIGGER trMyFirstTrigger
+ON Database
+FOR CREATE_TABLE, ALTER_TABLE, DROP_TABLE
+AS
+BEGIN
+Rollback
+Print 'You cannot create, alter or drop a table'
+END
