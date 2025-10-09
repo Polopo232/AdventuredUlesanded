4cc6c4fe21807f7c397c4b92e16efdf5ace29b8a
Polopo232 <nikiforovnikita08@gmail.com>
2025-10-09 15:46:47 +0300

    select käsk kui salesamount > 1000

--- WhereIsHaving.sql
+++ WhereIsHaving.sql
@@ -2,4 +2,9 @@ SELECT ProductKey, SUM(SalesAmount) AS TotalSales
 FROM FactInternetSales
 GROUP BY ProductKey
 
+SELECT ProductKey, SUM (SalesAmount) AS TotalSales
+FROM FactInternetSales
+GROUP BY ProductKey
+HAVING SUM (SalesAmount) > 1000
+
 SELECT * FROM FactInternetSales
