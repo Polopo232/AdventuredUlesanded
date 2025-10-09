d3206af2d200ef2a639f2614488e8dea85490885
Opilane TTHK <opilane@tthk.ee>
2025-10-09 14:11:31 +0300

    Kasutamine 2 Id ja loomine 2 Id

--- View_uuendused.sql
+++ View_uuendused.sql
@@ -4,3 +4,10 @@ Select Id, Name, Gender, Departmentld
 from tblEmployee
 
 SELECT * FROM vWEmployeesDataExceptSalary
+
+Update vWEmployeesDataExceptSalary
+Set Name = 'Mikey' Where Id = 2
+
+Delete from vWEmployeesDataExceptSalary where Id = 2
+Insert into vWEmployeesDataExceptSalary values (2, 'Mikey', 'Male', 2)
+
