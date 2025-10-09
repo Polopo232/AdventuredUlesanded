f90a3cd4a8435e5ed926f882289913dd087a2156
Polopo232 <nikiforovnikita08@gmail.com>
2025-10-09 15:17:00 +0300

    Loo uus tabelid ulesanne jaoks

---
+++ Except operaator.sql
@@ -0,0 +1,27 @@
+Create Table TableA
+(
+Id int primary key,
+Name nvarchar(50),
+Gender nvarchar(10)
+)
+
+
+Insert into TableA values (1, 'Mark', 'Male')
+Insert into TableA values (2, 'Mary', 'Female')
+Insert into TableA values (3, 'Steve', 'Male')
+Insert into TableA values (4, 'John', 'Male')
+Insert into TableA values (5, 'Sara', 'Female')
+
+
+Create Table TableB
+(
+Id int primary key,
+Name nvarchar(50),
+Gender nvarchar(10)
+)
+
+Insert into TableB values (4, 'John', 'Male')
+Insert into TableB values (5, 'Sara', 'Female')
+Insert into TableB values (6, 'Pam', 'Female')
+Insert into TableB values (7, 'Rebeka', 'Female')
+Insert into TableB values (8, 'Jordan', 'Male')
